019f39b8b662a43a693f754917a7eda4e41bb7fd
reimu <development@reimu.ws>
2023-03-24 10:09:08 -0500

    Move table logic to self

--- src/main.ts
+++ src/main.ts
@@ -18,66 +18,9 @@ const progressBar = document.getElementById(
 	"progress-bar"
 ) as HTMLProgressElement;
 const output = document.getElementById("output") as HTMLPreElement;
-const supportedFeaturesTable = document.getElementById(
-	"supported-features"
-) as HTMLTableSectionElement;
 
 let fileData: { name: string; data: FileData; type: string };
 
-// query for supported features
-(() => {
-	ffmpeg(
-		{ arguments: ["-codecs"] },
-		{
-			output: (data) => {
-				const codecRow =
-					/^ [D.][E.][VAS.][I.][L.][S.] [^=].+?\s+.+?/g.test(data);
-				if (!codecRow) return;
-
-				const [, support, abbreviation, ...name] = data.split(/ +/);
-				// check support length?
-				const decodingSupport = support[0] === "D";
-				const encodingSupport = support[1] === "E";
-				if (!decodingSupport && !encodingSupport) return;
-				const codecType =
-					support[2] === "V"
-						? "video"
-						: support[2] === "A"
-						? "audio"
-						: support[2] === "S"
-						? "subtitle"
-						: "unknown";
-				const intraFrameOnly = support[3] === "I";
-				const lossyCompression = support[4] === "L";
-				const losslessCompression = support[5] === "S";
-
-				const row = document.createElement("tr");
-				for (const value of [
-					abbreviation,
-					codecType,
-					name.join(" "),
-					decodingSupport,
-					encodingSupport,
-					intraFrameOnly,
-					lossyCompression,
-					losslessCompression,
-				]) {
-					const column = document.createElement("td");
-					column.textContent =
-						typeof value === "string"
-							? value
-							: value // assume it's a boolean
-							? "Yes"
-							: "No";
-					column.setAttribute("data-value", String(value));
-
-					row.appendChild(column);
-				}
-				supportedFeaturesTable.appendChild(row);
-			},
-		}
-	);
-})();
 
 function bootlegPopup(text: string) {
 	const popup = document.createElement("div");
